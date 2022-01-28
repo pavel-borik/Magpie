5730c46db99c93a9813085e205584bed63ce91aa
pavel-borik <pavel.borik1@gmail.com>
2022-01-28 19:38:10 +0100

    Dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.6.0"
     java
+    idea
     id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
@@ -13,19 +14,19 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
-    implementation("dev.kord:kord-core:0.8.0-M7")
-    implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("org.slf4j:slf4j-log4j12:1.7.32")
-    implementation("io.github.microutils:kotlin-logging:2.0.11")
+    implementation("dev.kord:kord-core:0.8.0-M8")
+    implementation("org.slf4j:slf4j-api:1.7.35")
+    implementation("org.slf4j:slf4j-log4j12:1.7.35")
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
     implementation("com.h2database:h2:2.1.210")
-    implementation("com.zaxxer:HikariCP:5.0.0")
+    implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.jetbrains.exposed:exposed-core:0.37.3")
     implementation("org.jetbrains.exposed:exposed-dao:0.37.3")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.37.3")
-    implementation("io.ktor:ktor-client-core:1.6.5")
-    implementation("io.ktor:ktor-client-cio:1.6.5")
-    implementation("io.ktor:ktor-client-jackson:1.6.5")
-    implementation("io.ktor:ktor-jackson:1.6.5")
+    implementation("io.ktor:ktor-client-core:1.6.7")
+    implementation("io.ktor:ktor-client-cio:1.6.7")
+    implementation("io.ktor:ktor-client-jackson:1.6.7")
+    implementation("io.ktor:ktor-jackson:1.6.7")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
