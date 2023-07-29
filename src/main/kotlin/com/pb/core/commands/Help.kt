3081325a1bd8c94dee112571a0365f8a7793bd09
pavel-borik <pavel.borik1@gmail.com>
2023-07-29 18:40:09 +0200

    Help will not be a slash command

--- src/main/kotlin/com/pb/core/commands/Help.kt
+++ src/main/kotlin/com/pb/core/commands/Help.kt
@@ -3,17 +3,11 @@ package com.pb.core.commands
 import com.pb.core.CommandRegistrationService
 import com.pb.core.data.ChatCommand
 import com.pb.core.data.ExecutionData
-import com.pb.core.data.SlashCommand
 import dev.kord.core.behavior.channel.createEmbed
-import dev.kord.core.behavior.interaction.response.DeferredPublicMessageInteractionResponseBehavior
-import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.entity.Message
-import dev.kord.core.entity.interaction.GuildChatInputCommandInteraction
-import dev.kord.rest.builder.interaction.GlobalChatInputCreateBuilder
 import dev.kord.rest.builder.message.EmbedBuilder
-import dev.kord.rest.builder.message.modify.embed
 
-class Help(private val commandRegistrationService: CommandRegistrationService) : ChatCommand, SlashCommand {
+class Help(private val commandRegistrationService: CommandRegistrationService) : ChatCommand {
     override val trigger = "help"
     override val isAdminOnly = false
     override val help = "!help"
@@ -22,14 +16,6 @@ class Help(private val commandRegistrationService: CommandRegistrationService) :
     override suspend fun execute(message: Message, executionData: ExecutionData) {
         message.channel.createEmbed { createEmbed() }
     }
-
-    override suspend fun execute(interaction: GuildChatInputCommandInteraction, response: DeferredPublicMessageInteractionResponseBehavior) {
-        response.respond {
-            embed { createEmbed() }
-        }
-    }
-
-    override fun buildInputCommand(builder: GlobalChatInputCreateBuilder) {}
 
     private fun EmbedBuilder.createEmbed() {
         title = "Commands"
